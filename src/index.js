37b91a2ee71f3ada78ba1c105c30d9be961b8d52
Michael Wong <emporophobe@gmail.com>
2018-08-06 09:57:11 -0400

    restyle, rename

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+// Load the top header on page load
 window.onload = function() {
 	var ajax = new XMLHttpRequest();
-	ajax.open("GET", "banner.html");
+	ajax.open("GET", "./components/header.html");
 	ajax.send();
 	// When the ajax request changes state
 	ajax.onreadystatechange = function () {
 		// When it's now complete
 		if (ajax.readyState === 4) {
-			// Inject the response into the #banner element
-			document.querySelector("#banner").innerHTML = ajax.response;
+			// Inject the response into the #header element
+			document.querySelector("#header").innerHTML = ajax.response;
 		}
 	}
 }
