3d5b0b2254f2b7302c25199113ea3cac84f0fe1a
Michael Wong <emporophobe@gmail.com>
2018-08-19 21:53:13 -0400

    <3 json

--- projects/bgg.js
+++ projects/bgg.js
@@ -1,23 +1,35 @@
-// use cors.io as a proxy to avoid cross origin request blocking (since there's no backend to make the request)
-const bgg_api_root = "https://cors.io?https://www.boardgamegeek.com/xmlapi2/";
+// JSON wrapper for BGG API, also doesn't have CORS restrictions
+const apiRoot = "https://cors.io?https://bgg-json.azurewebsites.net/"
 
 window.onload = function() {
 	document.getElementById("submit").onclick = bggSearch;
 }
 
-var bggSearch = function() { 
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", bgg_api_root + "collection/" + document.getElementById("bgg-username").innerText + "owned=1");
-	xhr.responseType = "document";
-	xhr.send();
-	// When the xhr request changes state
-	xhr.onreadystatechange = function () {
-		// When it's now complete
-		if (xhr.readyState === 4) {
-			// Inject the response into the #header element
-			console.log(document.getElementById("bgg-username").innerText);
-			console.log(xhr.responseXML);
-			document.querySelector("#result").innerHTML = xhr.body;
-		}
-	}
+var bggSearch = function() {
+	const url = apiRoot + "collection/" + document.getElementById("bgg-username").value + "?grouped=false";
+	document.getElementById("result").innerHTML = "🤔"
+	fetch(url)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(responseJson) {
+			const game = selectGame(responseJson);
+			document.getElementById("result").innerHTML = game.name;
+		})
+}
+
+var selectGame = function(gameJson) {
+	const numPlayers = document.getElementById("num-players").value;
+	const minDuration = document.getElementById("min-duration").value;
+	const maxDuration = document.getElementById("max-duration").value;
+	const validGames = gameJson.filter(game => 
+		game.owned
+		&& !game.isExpansion
+		&& game.minPlayers <= numPlayers
+		&& game.maxPlayers >= numPlayers
+		&& (!minDuration || game.playingTime >= minDuration)
+		&& (!maxDuration || game.playingTime <= maxDuration)
+	);
+	return validGames[Math.floor(Math.random()*validGames.length)];
+	
 }
